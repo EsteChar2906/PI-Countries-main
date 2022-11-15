f4434ba8597f0cc6aacb593ad5e99f479f482824
EsteChar2906 <ccarlospachecoc@gmail.com>
2022-11-14 22:18:26 -0500

    Deploy

--- api/config.js
+++ api/config.js
@@ -1,7 +1,11 @@
 
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "estefy";
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_NAME = process.env.DB_NAME || "countries";
-export const DB_PORT = process.env.DB_PORT || 5432;
-export const DB_URL = process.env.DB_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_PORT}:${DB_HOST}/${DB_NAME}`
+const DB_USER = process.env.DB_USER || "postgres";
+const DB_PASSWORD = process.env.DB_PASSWORD || "estefy";
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_NAME = process.env.DB_NAME || "countries";
+const DB_PORT = process.env.DB_PORT || 5432;
+const DB_URL = process.env.DB_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_PORT}:${DB_HOST}/${DB_NAME}`
+
+module.exports = {
+	DB_URL
+}
