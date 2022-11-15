253b74702e25b99da3d16225495d311d9cdd044a
EsteChar2906 <ccarlospachecoc@gmail.com>
2022-11-14 23:27:28 -0500

    DEPLOY

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
-
+const PORT = process.env.PORT || 3001;
 const DB_USER = process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || "estefy";
 const DB_HOST = process.env.DB_HOST || "localhost";
@@ -6,5 +6,5 @@ const DB_NAME = process.env.DB_NAME || "countries";
 const DB_PORT = process.env.DB_PORT || 5432;
 const DB_URL = process.env.DB_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-module.exports = { DB_URL };
+module.exports = { DB_URL, PORT };
 
--- api/index.js
+++ api/index.js
@@ -19,12 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { dataBase } = require('./src/dataBaseAxios/dataBaseAxios.js')
+const { dataBase } = require('./src/dataBaseAxios/dataBaseAxios.js');
+const { PORT } = require('./config.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     dataBase();
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
