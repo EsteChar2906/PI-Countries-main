beab172d6944f74a3f7d30abab0436f5e90eb8c1
EsteChar2906 <ccarlospachecoc@gmail.com>
2022-11-14 23:02:40 -0500

    Deploy

--- api/config.js
+++ api/config.js
@@ -4,5 +4,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD || "estefy";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "countries";
 const DB_PORT = process.env.DB_PORT || 5432;
-module.exports = process.env.DB_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const DB_URL = process.env.DB_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+module.exports = { DB_URL };
 
